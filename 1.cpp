0e4ed467cda874b41887569feb98b0c324a0f3f5
FeynmanLogic <112418441+FeynmanLogic@users.noreply.github.com>
2023-11-11 10:42:38 +0530

    Initial code

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+class Book
+{
+public:
+   char title[100];
+   char author[50];
+   int isbn;
+   bool availability; //1 is yes 0 if no
+   
+};
+
+
+class User
+{
+char name[10];
+
+};
+
